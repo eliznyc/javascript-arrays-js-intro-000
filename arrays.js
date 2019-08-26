c17578bb1bc5079be7e158d183b6c65aaa7d3b30
Elizabeth Peters <eliznyc@gmail.com>
2019-08-26 01:14:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,3 +19,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
     return (array[index]);
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array = array.shift;
+  return array;
+}
